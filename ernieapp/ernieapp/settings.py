ee8d4df577a505b706f64fa7885a25df8dac5b74
LauridsA <lauridsandersen2013@gmail.com>
2019-05-30 13:55:04 +0200

    added module in settings

--- ernieapp/ernieapp/settings.py
+++ ernieapp/ernieapp/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'challenge.apps.ChallengeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
