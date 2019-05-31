421c39f478aab18d876bd1c7720b8cd4df68f952
LauridsA <lauridsandersen2013@gmail.com>
2019-05-31 18:13:35 +0200

    pathing

--- ernieapp/ernieapp/urls.py
+++ ernieapp/ernieapp/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import sys
-sys.path.append('/challenge')
+sys.path.append('/challenge/')
 from django.contrib import admin
 from django.urls import path
 from models import Device, Task
